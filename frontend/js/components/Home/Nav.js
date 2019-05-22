732c47940d1c24e2c0ed09a9c5f33ff47ed55747
Jim Mac <jimmyenmanuel7@gmail.com>
2019-05-22 17:37:07 -0500

    arreglando el navbar

--- frontend/js/components/Home/Nav.js
+++ frontend/js/components/Home/Nav.js
@@ -38,9 +38,9 @@ class Nav extends Component {
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <a className="navbar-brand" href="/">TechFazt</a>
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                <div className="collapse navbar-collapse bg-light" id="navbarTogglerDemo01">
+                    {/*<a className="navbar-brand" href="/">TechFazt</a>*/}
+                    <ul className="navbar-nav ml-3 mt-2 mt-lg-0 ">
                         <li className="nav-item ">
                             <a className="nav-link" href="#">Servicios</a>
                         </li>
@@ -51,7 +51,7 @@ class Nav extends Component {
                             <a className="nav-link" href="#">Únete</a>
                         </li>
                     </ul>
-                    <form className="form-inline my-2 my-lg-0" onSubmit={this.onSubmit.bind(this)}>
+                    <form className="form-inline ml-3 my-2 my-lg-0" onSubmit={this.onSubmit.bind(this)}>
                         <input className="form-control mr-sm-2" type="email" placeholder="Email" name="email" value={this.state.email} onChange={this.obtenerCambio.bind(this)}></input>
                         <input className="form-control  mr-sm-2" type="password" placeholder="Contraseña" name="password" value={this.state.password} onChange={this.obtenerCambio.bind(this)}></input>
                         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Entrar</button>
